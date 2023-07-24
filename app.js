4b1808171e7267c9e45188aedf4f768763dad1f8
CH Chu <120808457+jiasyuanchu@users.noreply.github.com>
2023-07-24 14:26:46 +0800

    feat: basic router set up

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ if (process.env.NODE_ENV !== 'production') {
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const routes = require('./routes')
 
 const methodOverride = require('method-override')
 
@@ -19,6 +20,7 @@ app.use(methodOverride('_method'))
 
 app.use(express.urlencoded({ extended: true }))
 
+app.use(routes)
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
 module.exports = app

c949fade31b2768d61312dd9249012fa2b0f1668
av124773 <av124773@gmail.com>
2023-07-26 22:34:35 +0800

    feat: add signup

--- routes/apis/index.js
+++ routes/apis/index.js
@@ -2,7 +2,9 @@ const express = require('express')
 const router = express.Router()
 
 const { admin, followships, posts, trails, users } = require('../../models')
+const userController = require('../../controllers/apis/user-controller')
 
+router.use('/users', userController.signUp)
 router.use('/', (req, res) => res.send('this is home page.'))
 
 module.exports = router
--- services/user-services.js
+++ services/user-services.js
@@ -1,6 +1,35 @@
+const bcrypt = require('bcryptjs')
 const { User } = require('../models')
-const userServices = {
 
+const userServices = {
+  signUp: async (req, cb) => {
+    try {
+      const { name, email, password, passwordCheck } = req.body
+      if (password != passwordCheck) {
+        const err = new Error('Passwords do not match!')
+        err.status = 404
+        throw err
+      }
+
+      const user = await User.findOne({ where: { email: email } })
+      if (user) {
+        const err = new Error('Email already exists!')
+        err.status = 404
+        throw err
+      }
+
+      const hash = await bcrypt.hash(password, 10)
+      const newUser = await User.create({
+        name: name,
+        email: email,
+        password: hash
+      })
+      cb(null, { user: newUser })
+      
+    } catch (err) {
+      cb(err)
+    }
+  }
 }
 
 module.exports = userServices

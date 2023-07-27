0a8e705bd9852358b3357f9f69c2e9ee5482a441
av124773 <av124773@gmail.com>
2023-07-27 13:50:57 +0800

    feat: add error-handler

--- routes/apis/index.js
+++ routes/apis/index.js
@@ -3,8 +3,11 @@ const router = express.Router()
 
 const { admin, followships, posts, trails, users } = require('../../models')
 const userController = require('../../controllers/apis/user-controller')
+const { apiErrorHandler } = require('../../middleware/error-handler')
 
 router.post('/users', userController.signUp)
+
+router.use('/', apiErrorHandler)
 router.use('/', (req, res) => res.send('this is home page.'))
 
 module.exports = router

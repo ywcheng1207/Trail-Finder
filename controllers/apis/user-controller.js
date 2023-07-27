720921b8f965225c306d319f3066c0611e521043
av124773 <av124773@gmail.com>
2023-07-28 01:17:13 +0800

    feat: add signIn

--- controllers/apis/user-controller.js
+++ controllers/apis/user-controller.js
@@ -1,8 +1,11 @@
 const userServices = require('../../services/user-services')
 
 const userController = {
+  signIn: (req, res, next) => {
+    userServices.signIn(req, (err, data) => err ? next(err) : res.json({ status: 'success', data }))
+  },
   signUp: (req, res, next) => {
-    userServices.signUp(req, (err, data) => err ? next(err) : res.json({ status: 'success', data}))
+    userServices.signUp(req, (err, data) => err ? next(err) : res.json({ status: 'success', data }))
   }
 }
 

8daabeb4b6241c61138aec9fd25899740bf0ea6f
av124773 <av124773@gmail.com>
2023-08-01 11:12:18 +0800

    feat: add admin signIn

--- routes/apis/index.js
+++ routes/apis/index.js
@@ -3,15 +3,19 @@ const router = express.Router()
 
 const users = require('./modules/users')
 const posts = require('./modules/posts')
+const admin = require('./modules/admin')
 const userController = require('../../controllers/apis/user-controller')
+const adminController = require('../../controllers/apis/admin-controller')
 const { apiErrorHandler } = require('../../middleware/error-handler')
 const { signInAuth, isUser, isAdmin, authenticated } = require('../../middleware/auth')
 
 router.post('/users/signin', signInAuth, isUser, userController.signIn)
+router.post('/admin/signin', signInAuth, isAdmin, adminController.signIn)
 router.post('/users', userController.signUp)
 
-router.use('/users', authenticated, users)
-router.use('/posts', authenticated, posts)
+router.use('/admin', authenticated, isAdmin, admin)
+router.use('/users', authenticated, isUser, users)
+router.use('/posts', authenticated, isUser, posts)
 
 router.use('/', apiErrorHandler)
 router.use('/', (req, res) => res.send('this is home page.'))
--- services/admin-services.js
+++ services/admin-services.js
@@ -1,6 +1,21 @@
-const { } = require('../models')
-const adminServices = {
+const sequelize = require('sequelize')
+const jwt = require('jsonwebtoken')
+const { User, Post, Favorite, Followship } = require('../models')
 
+const adminServices = {
+  signIn: async (req, cb) => {
+    try {
+      const userData = req.user
+      delete userData.password
+      const token = jwt.sign(userData, process.env.JWT_SECRET, { expiresIn: '30d' })
+      cb(null, {
+        token,
+        user: userData
+      })
+    } catch (err) {
+      cb(err)
+    }
+  },
 }
 
 module.exports = adminServices

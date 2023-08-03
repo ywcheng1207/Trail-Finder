addfb53bd9d043ea84e2c584bc14b0e468f862eb
av124773 <av124773@gmail.com>
2023-08-03 16:01:28 +0800

    feat: add addfollowing and deletefollowing

--- routes/apis/index.js
+++ routes/apis/index.js
@@ -3,6 +3,7 @@ const router = express.Router()
 
 const users = require('./modules/users')
 const posts = require('./modules/posts')
+const followships = require('./modules/followships')
 const userController = require('../../controllers/apis/user-controller')
 const { apiErrorHandler } = require('../../middleware/error-handler')
 const { signInAuth, isUser, isAdmin, authenticated } = require('../../middleware/auth')
@@ -12,6 +13,7 @@ router.post('/users', userController.signUp)
 
 router.use('/users', authenticated, users)
 router.use('/posts', authenticated, posts)
+router.use('/followships', authenticated, followships)
 
 router.use('/', apiErrorHandler)
 router.use('/', (req, res) => res.send('this is home page.'))
--- routes/apis/modules/followships.js
+++ routes/apis/modules/followships.js
@@ -1,5 +1,11 @@
 const express = require('express')
 const router = express.Router()
+
 const followshipController = require('../../../controllers/apis/followship-controller')
+
+router.delete('/:userId', followshipController.deleteFollowing)
+router.post('/', followshipController.addFollowing)
+
+router.use('/', (req, res) => res.send('this is followship page.'))
 
 module.exports = router

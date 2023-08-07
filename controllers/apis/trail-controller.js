db0c8e18cc17e52b2f44da6d4558654d0904d664
CH Chu <120808457+jiasyuanchu@users.noreply.github.com>
2023-08-07 16:02:47 +0800

    fix: trail router sorting

--- controllers/apis/trail-controller.js
+++ controllers/apis/trail-controller.js
@@ -4,9 +4,6 @@ const trailController = {
   getAllTrails: (req, res, next) => {
     trailServices.getAllTrails(req, (err, data) => err ? next(err) : res.json(data))
   },
-  getTrailsGPX: (req, res, next) => {
-    trailServices.getTrailsGPX(req, (err, data) => err ? next(err) : res.json(data))
-  },
   getTrail: (req, res, next) => {
     trailServices.getTrail(req, (err, data) => err ? next(err) : res.json(data))
   },
--- routes/apis/modules/trails.js
+++ routes/apis/modules/trails.js
@@ -7,7 +7,6 @@ const { optionalAuthenticated } = require('../../../middleware/auth')
 
 router.get('/search', trailController.searchTrailByKeyword)
 router.get('/:trailId', optionalAuthenticated, trailController.getTrail)
-router.get('/gpx/:trailId', optionalAuthenticated, trailController.getTrailsGPX)
 router.get('/', optionalAuthenticated, trailController.getAllTrails)
 
 router.use('/', (req, res) => res.send('this is trail page.'))

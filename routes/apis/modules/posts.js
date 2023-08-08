a9e37c8c0f323ae8b496340811cd14c1364d8cb6
av124773 <av124773@gmail.com>
2023-08-08 10:30:23 +0800

    feat: modify authentication setting of the user router

--- routes/apis/modules/posts.js
+++ routes/apis/modules/posts.js
@@ -2,14 +2,8 @@ const express = require('express')
 const router = express.Router()
 
 const postController = require('../../../controllers/apis/post-controller')
-
-const { authenticated, optionalAuthenticated } = require('../../../middleware/auth')
-
-// router.get('/all', optionalAuthenticated, postController.getAllPosts)
-// router.get('/:postId', optionalAuthenticated, postController.getPost)
-// router.get('/', optionalAuthenticated, postController.getPosts)
-
 const upload = require('../../../middleware/multer')
+const { authenticated, optionalAuthenticated } = require('../../../middleware/auth')
 
 router.delete('/collects/:postId', authenticated, postController.deleteCollect)
 router.delete('/likes/:postId', authenticated, postController.deleteLike)
